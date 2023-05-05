ad57233bc9c30cce9ea8fcf9845f479b89c78811
eravinash-coder <akt7273922921@gmail.com>
2023-05-05 16:51:22 +0530

    first commit

---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+import { Form } from 'react-router-dom';
+
+export { default as Home } from './Home';
+export { default as Profile } from './Profile';
+export { default as CreateCampaign } from './CreateCampaign';
+export { default as CampaignDetails } from './CampaignDetails';
